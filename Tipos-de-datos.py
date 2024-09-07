87d03e814d4f563c45ef8b6cbccee3b0c279917a
Sof-IA-AFK <sofiprins6@gmail.com>
2024-09-06 21:41:46 -0500

    Cambio minúsculas iniciales por Mayúsculas

---
+++ Tipos-de-datos.py
@@ -0,0 +1,19 @@
+"string" #string se utiliza para cadena de textos
+'string' #asi tambien podemos generar una string
+"Hola mundo"
+'Hola mundo'
+
+"""Tus datos son:
+Nombre: Sofia
+Apellido: Muñoz"""
+'''Tus datos son:
+Nombre: Sofia
+Apellido: Muñoz'''
+
+#En python tenemos diferentes tipos de números
+4#número entero
+40.2#número tipo float
+#También tenemos el tipo de datos boolean
+True #Verdadero
+False #Falso
+
