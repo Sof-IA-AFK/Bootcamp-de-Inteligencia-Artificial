18bcc4b65d77bc3b03fc3ae7c2260c55685c8b32
Sof-IA-AFK <sofiprins6@gmail.com>
2024-09-21 10:59:27 -0500

    Abordamos los operadores de entrada y salida

--- Operadores_de_entrada_y_salida.py
+++ Operadores_de_entrada_y_salida.py
@@ -1,3 +1,10 @@
 nombre = input("¿Cuál es tu nombre?: ")
 print(nombre)
+print(type(nombre))
 
+edad = input("¿Cuál es tu edad?: ")
+print(edad)
+print(type(edad)) #El resultado de input siempre será string
+
+#Casting es el artificio que nos permite cambiar el tipo de dato
+#edad = float(input("¿Cuál es tu edad?: "))
