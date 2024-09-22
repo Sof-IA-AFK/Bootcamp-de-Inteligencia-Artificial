a705cb82384d30908129de6385d5e57a64a48c59
Sof-IA-AFK <sofiprins6@gmail.com>
2024-09-21 19:48:23 -0500

    Abordamos operaciones aritmeticas

---
+++ Operadores_aritmeticos.py
@@ -0,0 +1,23 @@
+#suma y resta (+ y -)
+suma = 12 + 4
+resta =12 - 4
+#multiplicación y división (* y /)
+multiplicacion = 12 * 4
+division = 12 / 4
+#potenciacion (**) exponente
+potenciacion = 12 ** 4
+#division entera
+division_entera = 12 // 4
+#resto (%) (modulo)
+resto = 12 % 4
+print(suma, resta, multiplicacion, division, potenciacion, division_entera, resto)
+tipo_de_dato_1 = type(["Hola profe", 20, 2004]) # devuelve el tipo de dato
+tipo_de_dato_2 = type(("Hola profe", 20, 2004))
+tipo_de_dato_3 = type(2.5)
+tipo_de_dato_4 = type(division)
+tipo_de_dato_5 = type(division_entera)
+print(tipo_de_dato_1)
+print(tipo_de_dato_2)
+print(tipo_de_dato_3)
+print(tipo_de_dato_4)
+print(tipo_de_dato_5)
